6c74c4b5b4a3f09d0b06a11dd7a10ad2d11a0d31
Kulandasamy <Chandrasekhar.Kulandasamy@experient-inc.com>
2019-01-11 01:43:35 -0500

    deleted Lib and Runner file

--- Runner/RDAPPHealthCheckrun.bat
+++
@@ -1,6 +0,0 @@
-set ProjectPath=N:\QA\LiveWorkSpace\HealthCheck
-echo %ProjectPath%
-set classpath=%ProjectPath%\bin;%ProjectPath%\lib\*
-echo %classpath%
-java org.testng.TestNG %ProjectPath%\RDTest.xml
-
